dd08ea22c1696481a54ad325a0d710df834de1b2
Tony <jozeftony46@gmail.com>
2024-02-28 20:35:33 +0530

    home page flash sale reposition

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <>
     <HomeContainerMain>
-        <FlashSale/>  
+        <FlashSale/>
          {/* <Cart/> */}
 
     </HomeContainerMain>
--- src/UI/FlashSaleStyled.jsx
+++ src/UI/FlashSaleStyled.jsx
@@ -1,87 +1,89 @@
 import styled from "styled-components";
 
 export const StyledFlashSale = {
-    backgroundColor: "#d4d4d4bd",
-    // margin:"100px",
-    // borderRadius:" 10px",
-    justifyContent:"center",
-    display:"flex",
-    alignItems:"center",
-    // flexDirection:"column",
-    // flexWrap:" wrap;",
-    height:"390px", 
-    width:"100%",
-    overflow:"hidden", //removed X
+//   backgroundColor: "#4b4b4b2a",
+  // margin:"100px",
+  // borderRadius:" 10px",
+  justifyContent: "center",
+  display: "flex",
+  alignItems: "center",
+  // flexDirection:"column",
+  // flexWrap:" wrap;",
+  height: "390px",
+  width: "90vw",
+  overflow: "hidden", //removed X
 
-    '@media(max-width:600px)':{
-        height:"525px",
-        width:"86vw", //added width 
-        display:"flex",
-        alignItems:"center",
-        marginLeft:"3px",
-        justifyContent:"center",
-        paddingLeft:"6.5px"
-    }
-}
+  "@media(max-width:600px)": {
+    height: "525px",
+    width: "90vw", //added width
+    display: "flex",
+    alignItems: "center",
+    // marginLeft:"3px",
+    justifyContent: "center",
+    paddingLeft: "6.5px",
+  },
+};
 export const FlashSaleH1 = {
-    // backgroundColor: "#34797e",
-    height:"45px", 
-    width:"170px",
-    display:"flex",
-    alignItems:"center",
-    justifyContent:"center",
+  // backgroundColor: "#34797e",
+  height: "45px",
+  width: "170px",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "start",
+  marginLeft: "20px",
 
-    
-    '@media (max-width:600px)': {
-        height:"62px", 
-        width:"120px",
-        marginLeft:"10px",
-    }
+  "@media (max-width:600px)": {
+    height: "45px",
+    width: "120px",
+    marginLeft: "40px",
+    marginRight: "30px",
+  },
+};
 
-}
-
-
-
-export const FSTypoH1=styled.h1`
-    font-size:22px;
-    font-weight:300;
-    @media (max-width: 600px) {
-        font-size:15px;
-    }
-`
+export const FSTypoH1 = styled.h1`
+  font-size: 22px;
+  font-weight: 300;
+  @media (max-width: 600px) {
+    font-size: 15px;
+  }
+`;
 export const HeadFS = {
-    // width:"100%",
-    // backgroundColor: "#17c2ce",
-    height:"45px", 
-    color:"#000000",
-    display:"flex",
-    alignItems:"center",
-    justifyContent:"center",
-}
-export const CountDownBox ={
-    marginLeft:"10px",
-}
+  // width:"100%",
+  // justifyContent:"start",
+  // alignItems:"start",
+//   backgroundColor: "#17c2ce",
+  height: "40px",
+  color: "#000000",
+  display: "flex",
+
+
+  "@media (max-width:1920px)": {
+    // width: "57vw",
+  },
+};
+export const CountDownBox = {
+ // backgroundColor: "#0b0a0a",
+  marginLeft: "10px",
+
+};
 export const ConDSpacer = {
-    fontSize:"22px",
-    marginTop:"10px",
-    marginRight:"5px",
-    '@media (max-width:600px)': {
-        marginTop:"18px",
-        marginRight:"1px",
-    }
-}
-export const CountDownFS ={
-    // backgroundColor: "#b91818",
-    height:"45px",
-    width:"40px",
-    color:"#000000",
-    '@media (max-width:600px)': {
-        marginTop:"7px",
-        height:"30px",
-        width:"25px",
-    }
-
-
-}
-
-
+  fontSize: "22px",
+  marginTop: "10px",
+  marginRight: "5px",
+  "@media (max-width:600px)": {
+    marginTop: "12px",
+    marginRight: "1px",
+  },
+};
+export const CountDownFS = {
+  // backgroundColor: "#b91818",
+  height: "45px",
+  width: "40px",
+  color: "#000000",
+  "@media (max-width:600px)": {
+    // marginTop:"7px",
+    height: "45px",
+    width: "25px",
+    // marginLeft:"30px",
+  },
+};
