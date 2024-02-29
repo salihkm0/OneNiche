1fc34c6c9b43502f911dbf7973dddf495aeee36c
Ameer Basha.K <bashabb738@gmail.com>
2024-02-29 16:29:01 +0530

    minor Update

--- src/Pages/ProductDetails.jsx
+++ src/Pages/ProductDetails.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import Feature from '../Components/Feature'
-import FirstBox from "../ProductPageComponents/FirstBox.jsx";
+import Feature from '../Components/Feature';
+import FirstBox from "../ProductPageComponents/FirstBox";
+import SimilarProductsBox from "../ProductPageComponents/SimilarProductsBox.jsx";
 
 export default function ProductDetails() {
   return <>
---
+++ src/ProductPageComponents/SimilarProductsBox.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import Cards from "../Common_Componets/Cards";
+
+
+export default function SimilarProductsBox(){
+    return(
+        <>
+        
+        </>
+    )
+}
