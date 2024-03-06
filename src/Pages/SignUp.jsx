75758c77ca0b929993abff122a437f52c00a560d
Muhammed Salih Km <salihkm000@gmail.com>
2024-03-06 20:30:13 +0530

    Code Arranged

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -1,32 +1,31 @@
-import React from "react";
-import * as Components from '../UI/SignUpStyle';
-function SignUp() {
-  const [signIn, toggle] = React.useState(true);
+import { useState } from "react";
+import * as Components from "../UI/SignUpStyle";
+
+
+export const SignUp = () => {
+  const [signIn, toggle] = useState(true);
   return (
     <Components.Container>
       <Components.SignUpContainer signinIn={signIn}>
         <Components.Form>
           <Components.Title>Create Account</Components.Title>
-          <Components.Input type='text' placeholder='Name' />
-          <Components.Input type='email' placeholder='Email' />
-          <Components.Input type='password' placeholder='Password' />
+          <Components.Input type="text" placeholder="Name" />
+          <Components.Input type="email" placeholder="Email" />
+          <Components.Input type="password" placeholder="Password" />
           <Components.Button>Sign Up</Components.Button>
         </Components.Form>
       </Components.SignUpContainer>
-
       <Components.SignInContainer signinIn={signIn}>
         <Components.Form>
           <Components.Title>Sign in</Components.Title>
-          <Components.Input type='email' placeholder='Email' />
-          <Components.Input type='password' placeholder='Password' />
-          <Components.Anchor href='#'>Forgot your password?</Components.Anchor>
+          <Components.Input type="email" placeholder="Email" />
+          <Components.Input type="password" placeholder="Password" />
+          <Components.Anchor href="#">Forgot your password?</Components.Anchor>
           <Components.Button>Sigin In</Components.Button>
         </Components.Form>
       </Components.SignInContainer>
-
       <Components.OverlayContainer signinIn={signIn}>
         <Components.Overlay signinIn={signIn}>
-
           <Components.LeftOverlayPanel signinIn={signIn}>
             <Components.Title>Welcome Back!</Components.Title>
             <Components.Paragraph>
@@ -36,7 +35,6 @@ function SignUp() {
               Sign In
             </Components.GhostButton>
           </Components.LeftOverlayPanel>
-
           <Components.RightOverlayPanel signinIn={signIn}>
             <Components.Title>Hello, Friend!</Components.Title>
             <Components.Paragraph>
@@ -46,12 +44,8 @@ function SignUp() {
               Sigin Up
             </Components.GhostButton>
           </Components.RightOverlayPanel>
-
         </Components.Overlay>
       </Components.OverlayContainer>
-
     </Components.Container>
-  )
+  );
 }
-
-export default SignUp;
