2d92f06340c6101fa11efdf4b7f90381a788b9ed
Muhammed Salih Km <salihkm000@gmail.com>
2024-03-10 20:28:51 +0530

    Cart price details added

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,21 +1,77 @@
-import { Bold, CartAddress, CartAddressButton, CartAddressContainer, CartLeftContainer, CartMainContainer, CartRightContainer, CartSubContainer } from "../UI/Cartstyle";
-
+import {
+  Bold,
+  CartAddress,
+  CartAddressButton,
+  CartAddressContainer,
+  CartLeftContainer,
+  CartMainContainer,
+  CartRightContainer,
+  CartRightSubContainer,
+  CartSubContainer,
+  DiscountDiscription,
+  PlaceOrderBtn,
+  PlaceOrderBtnContainer,
+  PriceListItem,
+  PriceListItemOff,
+  RightHead,
+  RightHeadContainer,
+  RightPriceList,
+  TotalAmt,
+  TotalAmtContainer,
+} from "../UI/Cartstyle";
 
 export const Cart = () => {
-  return(
-   <CartMainContainer>
+  return (
+    <CartMainContainer>
       <CartLeftContainer>
-          <CartAddressContainer>
-            <CartAddress>Deliver to: <Bold>Malappuram - 349023</Bold></CartAddress>
-            <CartAddressButton>Change</CartAddressButton>
-          </CartAddressContainer>
-          <CartSubContainer>
-            
-          </CartSubContainer>
+        <CartAddressContainer>
+          <CartAddress>
+            Deliver to: <Bold>Malappuram - 349023</Bold>
+          </CartAddress>
+          <CartAddressButton>Change</CartAddressButton>
+        </CartAddressContainer>
+        <CartSubContainer></CartSubContainer>
       </CartLeftContainer>
       <CartRightContainer>
-        Right
+        <CartRightSubContainer>
+          <RightHeadContainer>
+            <RightHead>Price Details</RightHead>
+          </RightHeadContainer>
+
+          <RightPriceList>
+            <PriceListItem>Price(2 items)</PriceListItem>
+            <PriceListItem>$1299.00</PriceListItem>
+          </RightPriceList>
+          <RightPriceList>
+            <PriceListItem>Discount</PriceListItem>
+            <PriceListItemOff>-$23.56</PriceListItemOff>
+          </RightPriceList>
+          <RightPriceList>
+            <PriceListItem>Coupons for you</PriceListItem>
+            <PriceListItemOff>-$9.00</PriceListItemOff>
+          </RightPriceList>
+          <RightPriceList>
+            <PriceListItem>Delivery Charges</PriceListItem>
+            <PriceListItemOff>Free</PriceListItemOff>
+          </RightPriceList>
+          <RightPriceList>
+            <PriceListItem>Secured Packaging Fee</PriceListItem>
+            <PriceListItem>$9.99</PriceListItem>
+          </RightPriceList>
+          <TotalAmtContainer>
+            <TotalAmt>Total Amount</TotalAmt>
+            <TotalAmt>$1459.35</TotalAmt>
+          </TotalAmtContainer>
+          <DiscountDiscription>
+          You will save $495.19 on this order
+          </DiscountDiscription>
+          <PlaceOrderBtnContainer>
+            <PlaceOrderBtn>
+            PLACE ORDER
+            </PlaceOrderBtn>
+          </PlaceOrderBtnContainer>
+        </CartRightSubContainer>
       </CartRightContainer>
     </CartMainContainer>
-  )
+  );
 };
--- src/UI/Cartstyle.jsx
+++ src/UI/Cartstyle.jsx
@@ -18,18 +18,20 @@ export const CartLeftContainer = styled.div`
   /* background-color: #a43737; */
   @media (max-width: 850px) {
     /* height: 60vh; */
+    padding: 0;
   }
 `;
 export const CartRightContainer = styled.div`
-  width: 60%;
-  padding: 0 20px;
-  /* border: 0.5px solid #828282c2; */
-  /* border-radius: 5px; */
-  /* height: 50vh; */
+  width: 50%;
+
+  /* padding: 10px 20px; */
+  /* border: 0.5px solid #828282c2;
+  border-radius: 5px; */
+  height: 70vh;
   /* background-color: #29a92e; */
   @media (max-width: 850px) {
     width: 100%;
-    height: 40vh;
+    height: auto;
   }
 `;
 export const CartAddressContainer = styled.div`
@@ -64,7 +66,105 @@ export const CartSubContainer = styled.div`
   width: 100%;
   height: 80vh;
   padding: 0 20px;
-  margin :  20px 0;
+  margin: 20px 0;
   border: 0.5px solid #828282c2;
   border-radius: 5px;
+`;
+export const RightHeadContainer = styled.div`
+  width: 100%;
+  padding: 0px 20px;
+  border-bottom: 0.5px solid #828282c2;
+  height: 50px;
+  /* background-color: #29a92e; */
+  display: flex;
+  align-items: center;
+  @media (max-width: 850px) {
+  }
+`;
+export const CartRightSubContainer = styled.div`
+  position: relative;
+  background-color: transparent;
+  /* background-color: #29a92e; */
+  width: 100%;
+  height: 60vh;
+  /* padding: 0 20px; */
+  border: 0.5px solid #828282c2;
+  border-radius: 5px;
+`;
+export const RightHead = styled.h3`
+  color: #9a9a9a;
+  font-size: 18px;
+  font-weight: 500;
+`;
+
+export const RightPriceList = styled.div`
+  width: 100%;
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+`;
+export const PriceListItem = styled.p`
+  color: #000000;
+  font-size: 16px;
+  font-weight: 400;
+`;
+export const PriceListItemOff = styled.p`
+  color: #23a902;
+  font-size: 16px;
+  font-weight: 400;
+`;
+export const TotalAmtContainer = styled.div`
+  width: 100%;
+  padding: 20px;
+  display: flex;
+  justify-content: space-between;
+  border-top: 0.5px dashed #828282c2;
+  border-bottom: 0.5px dashed #828282c2;
+  margin-top: 10px;
+`;
+export const TotalAmt = styled.p`
+  color: #000;
+  font-size: 18px;
+  font-weight: 500;
+`;
+export const DiscountDiscription = styled.p`
+  color: #158e0c;
+  font-size: 14px;
+  font-weight: 500;
+  padding: 10px;
+`;
+export const PlaceOrderBtnContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  /* width: 100%; */
+  height: auto;
+  /* background-color : #e0e000; */
+  padding: 10px;
+  position: absolute;
+  bottom: 0;
+  right :0;
+`;
+
+export const PlaceOrderBtn = styled.button`
+  background-color: #e06100;
+  color: white;
+  outline: none;
+  border: none;
+  cursor: pointer;
+  transition: all 0.3s ease;
+  padding: 10px 30px;
+  font-size: 18px;
+  font-weight: 500;
+  border-radius: 5px;
+  &:hover {
+    transform: scale(1.05);
+  }
+  &:active {
+    opacity: 0.7;
+  }
+  /* span {
+    font-size : 18px;
+    font-weight : bold;
+  } */
 `;
